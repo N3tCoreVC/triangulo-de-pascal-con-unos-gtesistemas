eb724bdab264e2a39f59f7a35c75ef5fc5a5ac29
Juan Pedro Osuna Velarde <jposuna57@gmail.com>
2018-09-21 18:01:53 -0500

    triangulo de pascal

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace triangulo_de_pascal_con_unos_gtesistemas
+{
+    class Program
+    {
+              static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hola, introduce el numero de unos de la base de la piramide:");
+            int tope = int.Parse(Console.ReadLine());
+
+            for(int i=1;i<=tope;i++)
+            {
+                for(int j=1;j<=(tope-i);j++) // escribir los espacios
+                Console.Write(" ");          // antes de los unos
+                for(int j=1;j<=i;j++)    // escribir los unos
+                    Console.Write("1 "); // con espacios intercalados
+                Console.Write("\n"); // terminó la línea, avanzar
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
